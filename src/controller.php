96b7e2791d7698e249614ac220f6ba3a5adfb5ef
Ben Thomas <bennyt9000@gmail.com>
2015-10-11 02:16:54 +0000

    Refactor for per-meet directories

--- src/controller.php
+++ src/controller.php
@@ -1,35 +1,38 @@
 <?php
-
 $app->get('/', function () use ($app) {
     return $app['twig']->render('index.html.twig');
 })->bind('homepage');
 
-$app->get('/entries', function () use ($app) {
-    return $app['twig']->render('entries.html.twig');
+$app->get('wintertrackfest/', function () use ($app) {
+    return $app['twig']->render('wintertrackfest/index.html.twig');
+})->bind('wintertrackfest_homepage');
+
+$app->get('wintertrackfest/entries', function () use ($app) {
+    return $app['twig']->render('wintertrackfest/entries.html.twig');
 })->bind('entries');
 
-$app->get('/heat-sheets', function () use ($app) {
-    return $app['twig']->render('heatSheets.html.twig');
+$app->get('wintertrackfest/heat-sheets', function () use ($app) {
+    return $app['twig']->render('wintertrackfest/heatSheets.html.twig');
 })->bind('heatSheets');
 
-$app->get('/records', function () use ($app) {
-    return $app['twig']->render('records.html.twig');
+$app->get('wintertrackfest/records', function () use ($app) {
+    return $app['twig']->render('wintertrackfest/records.html.twig');
 })->bind('records');
 
-$app->get('/payment', function () use ($app) {
-    return $app['twig']->render('payment.html.twig');
+$app->get('wintertrackfest/payment', function () use ($app) {
+    return $app['twig']->render('wintertrackfest/payment.html.twig');
 })->bind('payment');
 
-$app->get('/past-results', function () use ($app) {
-    return $app['twig']->render('entries.html.twig');
+$app->get('wintertrackfest/past-results', function () use ($app) {
+    return $app['twig']->render('wintertrackfest/entries.html.twig');
 })->bind('pastResults');
 
-$app->get('/rules', function () use ($app) {
-    return $app['twig']->render('entries.html.twig');
+$app->get('wintertrackfest/rules', function () use ($app) {
+    return $app['twig']->render('wintertrackfest/rules.html.twig');
 })->bind('rules');
 
-$app->get('/event-order', function () use ($app) {
-    return $app['twig']->render('eventOrder.html.twig');
+$app->get('wintertrackfest/event-order', function () use ($app) {
+    return $app['twig']->render('wintertrackfest/eventOrder.html.twig');
 })->bind('eventOrder');
 
 /**
@@ -43,7 +46,7 @@ for ($i = 8; $i <= 14; $i++) {
         $year = '20' . $i;
     }
 
-    $app->get('/results/' . $year, function () use ($app, $year) {
-        return $app['twig']->render('results/results'.$year.'.html.twig');
+    $app->get('wintertrackfest/results/' . $year, function () use ($app, $year) {
+        return $app['twig']->render('wintertrackfest/results/results'.$year.'.html.twig');
     })->bind('results'.$year);
 }
